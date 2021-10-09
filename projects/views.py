aee7764e6f25665e409398a743e057028b7173f6
ajitpawar411021@gmail.com <ajitpawar411021@gmail.com>
2021-10-09 13:29:05 +0530

    commit

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project
 
 projectlist =[
 
@@ -22,15 +23,12 @@ projectlist =[
 ]
 
 def projects(request):
-    page='projects'
-    number=10
-    context = {'page':page, 'number':number,'projects':projectlist}
+    projects = Project.objects.all()
+    context = {'projects':projects}
     return render(request, 'projects/projects.html', context )
 
 
 def project(request,pk):
-    projectobj = None 
-    for i in projectlist:
-        if i['id']== pk:
-            projectobj=i
-    return render(request, 'projects/single-project.html',{'project':projectobj})
+    projectobj = Project.objects.get(id=pk)
+   
+    return render(request, 'projects/single-project.html',{'project':projectobj })
