ebbdf9e50631d3036a86a1cade5898dc2089437a
ajitpawar411021@gmail.com <ajitpawar411021@gmail.com>
2021-10-09 15:07:53 +0530

    commit

--- projects/urls.py
+++ projects/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('',views.projects, name="projects"),
     path('project/<str:pk>/',views.project, name="project"),
 
+    path('create-project/',views.createProject, name="create-project"),
+
+
 ]
--- projects/views.py
+++ projects/views.py
@@ -2,25 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Project
 
-projectlist =[
-
-    {
-        'id':'1',
-        'title': "Ecommerce Website",
-        'description':'Fully functional ecommerce website'
-    },
-    {
-        'id':'2',
-        'title': "Portfolio Website",
-        'description':'This was a project where I built out my portfolio website'
-    },
-    {
-        'id':'3',
-        'title': "Social Network",
-        'description':'Awesome open source project I am still working on Social Network'
-    },
-
-]
 
 def projects(request):
     projects = Project.objects.all()
@@ -32,3 +13,8 @@ def project(request,pk):
     projectobj = Project.objects.get(id=pk)
    
     return render(request, 'projects/single-project.html',{'project':projectobj })
+
+
+def createProject(request):
+    context = {}
+    return render(request, "projects/project_form.html", context)
